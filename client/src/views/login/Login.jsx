7ee94a5fa85921dc90dced1b14817bb8447cbf47
Andres Nazzari <andresnazzari@gmail.com>
2022-06-24 15:31:26 +0900

    🚧 PROGRESS: starting login page

--- client/src/views/login/Login.jsx
+++ client/src/views/login/Login.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import Alert from '../../components/Alert';
+import styles from './style/Login.module.scss';
+import logoLogin from './assets/logo-login.png';
 
 const Login = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return <div>Login</div>;
 };
 
