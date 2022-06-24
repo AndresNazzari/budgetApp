045eb933f2a8d24b6f3a72ae272c9cc8178fa06d
Andres Nazzari <andresnazzari@gmail.com>
2022-06-24 15:55:43 +0900

    ✨ NEW FEATURE: Add  ealert component

--- client/src/components/alert/Alert.jsx
+++ client/src/components/alert/Alert.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import './style/alert.css';
 
 const Alert = () => {
-    return <div>Alert</div>;
+    const alerts = useSelector((store) => store.alertsReducer);
+
+    return (
+        <div className='alerts-container'>
+            {alerts !== null &&
+                alerts.map((alert) => (
+                    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+                        {alert.message}
+                    </div>
+                ))}
+        </div>
+    );
 };
 
 export default Alert;
