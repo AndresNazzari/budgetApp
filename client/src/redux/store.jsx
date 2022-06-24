de6e8d05165b52dcb4721fafdaf427f109bd9e5c
Andres Nazzari <andresnazzari@gmail.com>
2022-06-24 16:03:37 +0900

    ✨ NEW FEATURE: Add  Categories state and actions

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userSlice } from './states/user.state';
+import { categorySlice } from './states/category.state';
 import { alertSlice } from './states/alert.state';
 
 export default configureStore({
-    reducer: { userReducer: userSlice.reducer, alertsReducer: alertSlice.reducer },
+    reducer: {
+        userReducer: userSlice.reducer,
+        categoryReducer: categorySlice.reducer,
+        alertsReducer: alertSlice.reducer,
+    },
 });
