4198556b606b1d3ff16a4022fced163c922cf979
Andres Nazzari <andresnazzari@gmail.com>
2022-06-24 16:05:37 +0900

    ✨ NEW FEATURE: Add reducers to store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userSlice } from './states/user.state';
+import { expenseSlice } from './states/expense.state';
+import { incomeSlice } from './states/income.state';
 import { categorySlice } from './states/category.state';
 import { alertSlice } from './states/alert.state';
 
 export default configureStore({
     reducer: {
         userReducer: userSlice.reducer,
+        expenseReducer: expenseSlice.reducer,
+        incomeReducer: incomeSlice.reducer,
         categoryReducer: categorySlice.reducer,
         alertsReducer: alertSlice.reducer,
     },
