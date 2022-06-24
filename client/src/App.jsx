eed5d8752c0c0c3436e97ad6975af3c39bada405
Andres Nazzari <andresnazzari@gmail.com>
2022-06-24 15:50:18 +0900

    🚧 PROGRESS: creating routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import setAuthToken from './util/setAuthToken';
+import { loadUserAction } from './redux/actions/user.action';
+import { setToken } from './redux/states/user.state';
+
+import Login from './components/login/Login';
+import Signup from './components/signup/Signup';
+
+if (localStorage.userToken) {
+    setAuthToken(localStorage.userToken);
+    store.dispatch(setToken({ userToken: localStorage.userToken }));
+}
 
 const App = () => {
-    return <div>App</div>;
+    useEffect(() => {
+        store.dispatch(loadUserAction());
+    }, []);
+
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path='/' element={<Login />} />
+                    <Route exact path='/signup' element={<Signup />} />
+
+                    <Route path='*' element={<Login />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    );
 };
 
 export default App;
