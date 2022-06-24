37d56978d2d3acfbc6795729ed81f0a843fa0174
Andres Nazzari <andresnazzari@gmail.com>
2022-06-24 15:48:39 +0900

    🚧 PROGRESS: store

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { userSlice } from './states/user.state';
+import { alertSlice } from './states/alert.state';
 
 export default configureStore({
-    reducer: {},
+    reducer: { userReducer: userSlice.reducer, alertsReducer: alertSlice.reducer },
 });
